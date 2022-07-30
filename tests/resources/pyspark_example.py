fa9f789b8adc9d53140d75b9d14baea9f8073096
PApostol <pav.apostolidis@gmail.com>
2022-07-30 14:23:38 +0100

    Update tests

--- tests/resources/pyspark_example.py
+++ tests/resources/pyspark_example.py
@@ -12,11 +12,13 @@ accuracy = int(sys.argv[1]) if len(sys.argv) > 1 else 50000
 partitions = 2
 n = accuracy * partitions
 
+
 def f(_):
     x = random() * 2 - 1
     y = random() * 2 - 1
-    return 1 if x ** 2 + y ** 2 <= 1 else 0
+    return 1 if x**2 + y**2 <= 1 else 0
+
 
 count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-print('Pi is about {0}'.format(4.0 * count / n))
+print(f'Pi is about {4.0 * count / n}')
 spark.stop()
--- tests/run_test.py
+++ tests/run_test.py
@@ -2,11 +2,11 @@
 from spark_submit import SparkJob
 
 print('---Running example on local mode---')
-job = SparkJob('tests/resources/pyspark_example.py', main_file_args ='100000')
+job = SparkJob('tests/resources/pyspark_example.py', main_file_args='100000')
 
 print(f'spark-submit command:\n {job.get_submit_cmd(multiline=True)}')
 print('\nJob running...\n')
-job.submit()
+job.submit(use_env_vars=True)
 
 print(f'Job output:\n{job.get_output()}\n')
 print(f'Job state: {job.get_state()}\n')
