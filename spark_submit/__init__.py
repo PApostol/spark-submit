a757df3edb746d0db58712b16b3074c837e4f045
PApostol <pav.apostolidis@gmail.com>
2022-07-30 14:20:11 +0100

    Minor refactoring

--- spark_submit/__init__.py
+++ spark_submit/__init__.py
@@ -1,8 +1,16 @@
 """spark-submit module"""
 
-from .__info__ import (__author__, __author_email__, __bugtrack_url__,
-                       __description__, __license__, __maintainer__, __title__,
-                       __url__, __version__)
+from .__info__ import (
+    __author__,
+    __author_email__,
+    __bugtrack_url__,
+    __description__,
+    __license__,
+    __maintainer__,
+    __title__,
+    __url__,
+    __version__,
+)
 from .sparkjob import SparkJob
 from .system import system_info
 
